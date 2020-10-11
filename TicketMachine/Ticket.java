fbc43d49cb2c5040faaafe8fd9fab7cce5616afd
Ayarisu <luiscarlosmatossilva@hotmail.com>
2020-10-11 01:47:39 +0100

    ticket

---
+++ TicketMachine/Ticket.java
@@ -0,0 +1,13 @@
+/**
+ * Write a description of class Ticket here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Ticket
+{
+    // Default ticket prices for locations
+    private String aylesbury_Ticket;
+    private String amersham_Ticket;
+    private String highWycombe_Ticket;
+}
